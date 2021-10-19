70c264955b1cbbb1ae6ab095a30a0e4487f2a950
Paul McClean <pmcclean@petsafe.net>
2021-10-19 11:46:01 +0100

    Update wording

--- src/pages/static.js
+++ src/pages/static.js
@@ -2,7 +2,7 @@ import * as React from "react"
 const StaticPage = () => (
   <main>
     <h1>SSR Page with Dogs</h1>
-    <p>Page loads image dynamically from dog photo API</p>
+    <p>Page loads static image URL</p>
     <img
       alt="Happy dog"
       src="https://images.dog.ceo/breeds/ovcharka-caucasian/IMG_20190811_105047.jpg"
